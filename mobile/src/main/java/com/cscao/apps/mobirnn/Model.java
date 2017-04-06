2abe3283fabacc3a6c529681ae81334cb96060d8
Arron Cao <qqcao.cs@gmail.com>
2017-04-06 01:34:56 -0400

    add: real model data

--- mobile/src/main/java/com/cscao/apps/mobirnn/Model.java
+++ mobile/src/main/java/com/cscao/apps/mobirnn/Model.java
@@ -8,4 +8,33 @@ package com.cscao.apps.mobirnn;
 
 public class Model {
 
+    float[][][] weights;
+    float[][] biases;
+    float[][] w_in;
+    float[][] w_out;
+    float[] b_in;
+    float[] b_out;
+
+    public Model() {
+    }
+
+    public Model(float[][][] weights, float[][] biases, float[][] w_in, float[][] w_out,
+            float[] b_in,
+            float[] b_out) {
+        this.weights = weights;
+        this.biases = biases;
+        this.w_in = w_in;
+        this.w_out = w_out;
+        this.b_in = b_in;
+        this.b_out = b_out;
+    }
+
+    public Model(String dataFolder) {
+        String bInPath = dataFolder + "b_in.csv";
+        String bOutPath = dataFolder + "b_out.csv";
+        String wInPath = dataFolder + "w_in.csv";
+        String wOutPath = dataFolder + "w_out.csv";
+
+
+    }
 }
